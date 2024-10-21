9394d6591629089c7d5d12b0d10c55133bcb902e
AguzzTN54 <gusagusnanang@gmail.com>
2024-10-21 23:31:26 +0700

    Use 3rd party Youtube Provider

--- src/routes/track/+server.js
+++ src/routes/track/+server.js
@@ -1,5 +1,4 @@
 import { json } from '@sveltejs/kit';
-import { Innertube, UniversalCache } from 'youtubei.js';
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET() {
@@ -7,14 +6,13 @@ export async function GET() {
 }
 
 /** @type {import('./$types').RequestHandler} */
-export async function POST({ request, fetch }) {
+export async function POST({ request }) {
 	try {
 		const { videoID, type = 'audio' } = await request.json();
 		const vID = videoID?.replace(/[^A-Za-z0-9_\\-]/g, '');
 		if (!vID) return json({ message: 'please input video ID' }, { status: 400 });
 
-		// const videoResult = await mp3Youtube(vID, fetch);
-		const videoResult = await proccessYt(vID, type);
+		const videoResult = await y2meta(vID, type);
 
 		return json({ message: 'ok', ...videoResult }, { status: 200 });
 	} catch (e) {
@@ -23,23 +21,32 @@ export async function POST({ request, fetch }) {
 	}
 }
 
-const proccessYt = async (vid, type) => {
-	const yt = await Innertube.create({
-		cache: new UniversalCache(false),
-		generate_session_locally: true
+const y2meta = async (vid, type) => {
+	const isVideo = type !== 'audio';
+	const quality = isVideo ? '360' : '320';
+	const format = isVideo ? 'mp4' : 'mp3';
+
+	const response = await fetch('https://6fee.mmnm.store/oajax.php', {
+		headers: {
+			'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+			Origin: 'https://6fee.mmnm.store'
+		},
+		body: `videoid=${vid}&downtype=${format}&vquality=${quality}`,
+		method: 'POST'
 	});
 
-	const ytInfo = await yt.getBasicInfo(vid);
-	const format = ytInfo.chooseFormat({ type, quality: type !== 'audio' ? '360p' : 'best' });
-	const url = format?.decipher(yt.session.player);
-	if (!format) throw new Error('Format Error');
-
-	const result = {
-		formats: { download: url },
-		author: ytInfo.basic_info.author,
-		title: ytInfo.basic_info.title,
-		images: ytInfo.basic_info.thumbnail
-	};
-
-	return result;
+	const { url, filename } = await response.json();
+	const images = [
+		{
+			width: 180,
+			height: 320,
+			url: `https://img.youtube.com/vi/${vid}/mqdefault.jpg`
+		},
+		{
+			width: 1280,
+			height: 720,
+			url: `https://img.youtube.com/vi/${vid}/maxresdefault.jpg`
+		}
+	];
+	return { download: url, images, title: filename };
 };
