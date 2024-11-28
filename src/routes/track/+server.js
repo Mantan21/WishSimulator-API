cec640a9d2e3d868f5724012ce06820081682c3d
AguzzTN54 <gusagusnanang@gmail.com>
2024-11-28 22:10:11 +0700

    Remove cnvmp3 provider

--- src/routes/track/+server.js
+++ src/routes/track/+server.js
@@ -2,7 +2,6 @@ import { json } from '@sveltejs/kit';
 import { submagic } from './providers/submagic';
 import { freemake } from './providers/freemake';
 import { transkriptor } from './providers/transkriptor';
-import { cnvmp3 } from './providers/cnvmp3';
 import { yt5s } from './providers/yt5s';
 import { onedmp3 } from './providers/1dmp3';
 import { mediamister } from './providers/mediamister';
@@ -28,8 +27,8 @@ export async function POST({ request, fetch }) {
 }
 
 const proccessYT = (vid, type, fetch) => {
-	const videoProvider = [cnvmp3, submagic, freemake, transkriptor, mediamister, yt5s, onedmp3];
-	const audioProvider = [cnvmp3, submagic, freemake];
+	const videoProvider = [submagic, freemake, transkriptor, mediamister, yt5s, onedmp3];
+	const audioProvider = [submagic, freemake];
 	const provider = type !== 'audio' ? videoProvider : audioProvider;
 	const ytFn = provider[Math.floor(Math.random() * provider.length)];
 	return ytFn(vid, type, fetch);
